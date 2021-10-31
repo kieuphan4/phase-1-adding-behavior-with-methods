bc526b7a5c1bd02215930ad9083566f8bef4023d
Kieu <kieuphan530@gmail.com>
2021-10-31 18:50:31 -0500

    commit tree 742cced01ebcb03c2b1f09736b820f0675b0cbf1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+// orangeCat = new Cat("Christina", "male")
+// console.log(orangeCat.speak());
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+// corgiDog = new Dog("Max", "female")
+// console.log(corgiDog.speak());
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if (this.sex === "male") {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
+
+// talkingBird = new Bird("Mable", "male")
+// console.log(talkingBird.speak());
